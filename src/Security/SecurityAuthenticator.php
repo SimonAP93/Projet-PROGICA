2bf05867de8624db6ab149998ad72a346a2d2bbe
simon aposhian <simon.aposhian@gmail.com>
2021-06-29 16:32:04 +0200

    admin access control

--- src/Security/SecurityAuthenticator.php
+++ src/Security/SecurityAuthenticator.php
@@ -51,7 +51,7 @@ class SecurityAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('admin_index'));
+        return new RedirectResponse($this->urlGenerator->generate('home'));
         
     }
 
